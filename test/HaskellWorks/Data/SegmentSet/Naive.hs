fac565edfbe83713c7d542c80a781b098d47b1ce
John Ky <newhoggy@gmail.com>
2017-05-01 14:10:36 +1000

    Use enum instance instead of num (#19)
    * Use different test that doesn't require subtraction
    
    * Use Enum typeclass instance of Num

--- test/HaskellWorks/Data/SegmentSet/Naive.hs
+++ test/HaskellWorks/Data/SegmentSet/Naive.hs
@@ -18,19 +18,19 @@ newtype SegmentSet a = SegmentSet [Segment a] deriving (Show, Eq)
 empty :: SegmentSet a
 empty = SegmentSet []
 
-fromList :: (Ord a, Num a) => [Segment a] -> SegmentSet a
+fromList :: (Ord a, Enum a) => [Segment a] -> SegmentSet a
 fromList = foldr' update empty
 
 toList :: SegmentSet a -> [Segment a]
 toList (SegmentSet as) = as
 
-update :: (Ord a, Num a) => Segment a -> SegmentSet a -> SegmentSet a
+update :: (Ord a, Enum a) => Segment a -> SegmentSet a -> SegmentSet a
 update i (SegmentSet as) =
   let (ls, b1, b2, rs)  = splitSegment i as
       i'                = merge b1 i b2
    in SegmentSet $ ls ++ (i':rs)
 
-remove :: (Ord a, Num a) => Segment a -> SegmentSet a -> SegmentSet a
+remove :: (Ord a, Enum a) => Segment a -> SegmentSet a -> SegmentSet a
 remove i (SegmentSet as) =
   let (ls, b1, b2, rs) = splitSegment i as
       b1s = maybe [] (minus i) b1
@@ -50,14 +50,13 @@ splitSegment (Segment s e) as = (ls, b1, b2, rs)
 overlaps :: Ord a => Segment a -> Segment a -> Bool
 overlaps (Segment s1 e1) (Segment s2 e2) = if s1 <= s2 then e1 >= s2 else e2 >= s1
 
-minus :: (Ord a, Num a) => Segment a -> Segment a -> [Segment a]
+minus :: (Ord a, Enum a) => Segment a -> Segment a -> [Segment a]
 minus (Segment s e) (Segment fs fe) =
-  let (ds, de) = (max s fs - fs, fe - min e fe)
-      as = if ds == 0 then Nothing else Just (Segment  fs           (fs + ds - 1 ))
-      bs = if de == 0 then Nothing else Just (Segment (fe - de + 1)  fe           )
+  let as = if s <= fs then Nothing else Just (Segment  fs      (pred s))
+      bs = if e >= fe then Nothing else Just (Segment (succ e)  fe     )
   in catMaybes [as, bs]
 
-merge :: (Ord a, Num a) => Maybe (Segment a) -> Segment a -> Maybe (Segment a) -> Segment a
+merge :: (Ord a, Enum a) => Maybe (Segment a) -> Segment a -> Maybe (Segment a) -> Segment a
 merge (Just (Segment sb1 _))    (Segment s e) (Just (Segment _ eb2))   = Segment (min sb1 s) (max e eb2)
 merge Nothing                   (Segment s e) (Just (Segment sb2 eb2)) = Segment (min sb2 s) (max e eb2)
 merge (Just (Segment sb1 eb2))  (Segment s e) Nothing                  = Segment (min sb1 s) (max e eb2)
@@ -65,4 +64,4 @@ merge _ i _                                                            = i
 
 uncons' :: [a] -> (Maybe a, [a])
 uncons' []     = (Nothing, [])
-uncons' (a:as) = (Just a, as)
+uncons' (a:as) = (Just a , as)
