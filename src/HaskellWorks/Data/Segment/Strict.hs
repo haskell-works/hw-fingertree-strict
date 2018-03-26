bb9022f3f0fac99344d0b6209a1bf08636ced3f9
John Ky <newhoggy@gmail.com>
2018-03-26 15:52:37 +1100

    Add deepseq support

--- src/HaskellWorks/Data/Segment/Strict.hs
+++ src/HaskellWorks/Data/Segment/Strict.hs
@@ -1,14 +1,18 @@
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module HaskellWorks.Data.Segment.Strict where
 
+import Control.DeepSeq                     (NFData)
+import GHC.Generics                        (Generic)
 import HaskellWorks.Data.FingerTree.Strict
 
 -- | A closed segment.  The lower bound should be less than or equal
 -- to the higher bound.
 data Segment k = Segment { low :: !k, high :: !k }
-    deriving (Eq, Ord, Show)
+    deriving (Eq, Ord, Show, Generic, NFData)
 
 -- | A segment in which the lower and upper bounds are equal.
 point :: k -> Segment k
